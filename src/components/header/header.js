4dbec15921a9bd739e328708c8d8293c922ee200
Eugene <eshereverov@gmail.com>
2019-04-05 23:09:09 +0300

    refactor swapiservices

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,23 +5,23 @@ export default function Header() {
   return (
     <div className='header d-flex'>
       <h3>
-        <a href='#'>
+        <a href='/'>
           Star DB
         </a>
       </h3>
       <ul className='d-flex'>
         <li>
-          <a href='#'>
+          <a href='/'>
             People
           </a>
         </li>
         <li>
-          <a href='#'>
+          <a href='/'>
             Planets
           </a>
         </li>
         <li>
-          <a href='#'>
+          <a href='/'>
             Starships
           </a>
         </li>
--- src/services/swapi-services.js
+++ src/services/swapi-services.js
@@ -11,28 +11,70 @@ export default class SWapiService {
 
   async getAllPeople() {
     const res = await this.getResources('/people/');
-    return res.results;
+    return res.results.map(this._transformPerson);
   }
 
-  getPeople(id) {
-    return this.getResources(`/people/${id}/`)
+  async getPeople(id) {
+    const person = await this.getResources(`/people/${id}/`);
+    return this._transformPerson(person);
   }
 
   async getAllPlanets() {
     const res = await this.getResources('/planets/');
-    return res.results;
+    return res.results.map(this._transformPlanet);
   }
 
-  getPlanet(id) {
-    return this.getResources(`/planets/${id}/`)
+  async getPlanet(id) {
+    const planet = await this.getResources(`/planets/${id}/`);
+    return this._transformPlanet(planet);
   }
 
   async getAllStarships() {
     const res = await this.getResources('/starships/');
-    return res.results;
+    return res.results.map(this._transformStarship);
   }
 
-  getStarship(id) {
-    return this.getResources(`/starships/${id}/`)
+  async getStarship(id) {
+    const starship = await this.getResources(`/starships/${id}/`);
+    return this._transformStarship(starship);
+  }
+
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  _transformPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter,
+    }
+  }
+
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor,
+    }
+  }
+
+  _transformStarship(starship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passenger: starship.passenger,
+      cargoCapasity: starship.cargoCapasity,
+    }
   }
 }
