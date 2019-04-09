bfd131d623fdf722cbfe557537be20a6e5cf7c0a
Eugene <eshereverov@gmail.com>
2019-04-09 20:55:08 +0300

    add people list from api functionality

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './item-list.css';
+import Spinner from '../spinner';
 
-export default function ItemList() {
-  return (
-    <ul className="item-list list-group">
-      <li className="list-group-item">
-        Luke Skywalker
-      </li>
-      <li className="list-group-item">
-        Darth Vader
-      </li>
-      <li className="list-group-item">
-        R2-D2
-      </li>
-    </ul>
-  )
+import SWapiService from '../../services/swapi-services'
+
+export default class ItemList extends Component {
+  swapiService = new SWapiService()
+
+  state = {
+    peopleList: null
+  }
+
+  componentDidMount() {
+    this.swapiService
+      .getAllPeople()
+      .then((peopleList) => {
+        this.setState({peopleList})
+      })
+  }
+
+  RenderItems(arr) {
+    return arr.map((person) => {
+      return (
+        <li className="list-group-item"
+            key={person.id}
+            onClick={ () => this.props.onItemSelected(person.id) }>
+          {person.name}
+        </li>
+      )
+    })
+  }
+
+  render() {
+    const { peopleList } = this.state
+    return (
+      <ul className="item-list list-group">
+        { peopleList ? this.RenderItems(peopleList) : <Spinner /> }
+      </ul>
+    )
+  }
 }
