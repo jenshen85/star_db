da7465ec0fcfb5babed3f6362ced6697f81826e7
Eugene <eshereverov@gmail.com>
2019-04-09 20:19:50 +0300

    add disable random planet button

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './header';
 import RandomPlanet from './random-planet';
@@ -7,20 +7,43 @@ import PersonDetails from './person-details';
 
 import './App.css';
 
-export default function App() {
-  return (
-    <div className='container'>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
+  state = {
+    showRandomPlanet: true
+  }
 
-      <div className='row mb2'>
-        <div className='col-md-6'>
-          <ItemList />
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    })
+  }
+
+  render() {
+
+    return (
+      <div className='container'>
+        <Header />
+        { this.state.showRandomPlanet ? <RandomPlanet /> : null }
+
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
         </div>
-        <div className='col-md-6'>
-          <PersonDetails />
+
+        <div className='row mb2'>
+          <div className='col-md-6'>
+            <ItemList />
+          </div>
+          <div className='col-md-6'>
+            <PersonDetails />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
